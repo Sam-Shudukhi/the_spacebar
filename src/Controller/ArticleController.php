75c844941a7b466d358b220a89f00ca8227c4641
Sam Shudukhi <alshuduki@gmail.com>
2018-08-04 23:00:37 -0600

    Added a slug controller action

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -9,11 +9,25 @@
 namespace App\Controller;
 
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Response;
 
 class ArticleController
 {
+    /**
+     * @Route("/")
+     */
     public function homepage(){
         return new Response('First Page of The Site');
+    }
+
+    /**
+     * @Route("/news/{slug}")
+     */
+    public  function  show($slug){
+        return new Response(sprintf(
+                "Future Page To Show The Article: %s",
+                $slug
+        ));
     }
 }
