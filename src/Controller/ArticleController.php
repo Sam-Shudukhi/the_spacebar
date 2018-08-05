f8e9b24fa6377eec830641cbfe1bf80368a9395e
Sam Shudukhi <alshuduki@gmail.com>
2018-08-05 00:33:58 -0600

    render a page for articles + a template that inherits a base content

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -10,9 +10,10 @@ namespace App\Controller;
 
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
-class ArticleController
+class ArticleController extends AbstractController
 {
     /**
      * @Route("/")
@@ -25,9 +26,16 @@ class ArticleController
      * @Route("/news/{slug}")
      */
     public  function  show($slug){
-        return new Response(sprintf(
-                "Future Page To Show The Article: %s",
-                $slug
-        ));
+//        User comments
+        $comments = [
+            "I had a normal bacon once and it didn't taste like bacon",
+            "This is awesome. I love me some tacos",
+            "I love tacos too. Buy some from my site! tacoserria.com"
+        ];
+
+        return $this->render("article/show.html.twig",[
+            'title' => ucwords(str_replace('-',' ',$slug)),
+            'comments' => $comments
+        ]);
     }
 }
